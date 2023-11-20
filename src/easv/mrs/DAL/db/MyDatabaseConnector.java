f78d76a836ffd6625e88e47d1b928fcadf420276
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2023-11-20 10:45:32 +0100

    Data Source etc.

--- src/easv/mrs/DAL/db/MyDatabaseConnector.java
+++ src/easv/mrs/DAL/db/MyDatabaseConnector.java
@@ -1,21 +1,28 @@
 package easv.mrs.DAL.db;
 
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MyDatabaseConnector {
 
 
     //Class will easv.mrs.be included when we start working on DATABASES
-    /*
+
     private SQLServerDataSource dataSource;
 
     public MyDatabaseConnector()
     {
         dataSource = new SQLServerDataSource();
-        dataSource.setServerName("");
-        dataSource.setDatabaseName("");
-        dataSource.setUser("");
-        dataSource.setPassword("");
+        dataSource.setServerName("10.176.111.34");
+        dataSource.setDatabaseName("SMSJ_MRS2023");
+        dataSource.setUser("CSe2023a_e_39");
+        dataSource.setPassword("CSe2023aE39#23");
         dataSource.setPortNumber(1433);
+        dataSource.setTrustServerCertificate(true);
     }
 
     public Connection getConnection() throws SQLServerException {
@@ -32,7 +39,10 @@ public class MyDatabaseConnector {
             System.out.println("Is it open? " + !connection.isClosed());
 
         } //Connection gets closed here
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    */
+
 
 }
